eb2b2e5887eb18709b7272b0327d3563ba783b1a
chong wy <woonyap616@gmail.com>
2024-12-29 20:59:18 +0800

    remove watermark

--- Project/React_RoomRentalProject/src/components/shared/FileUploader.js
+++ Project/React_RoomRentalProject/src/components/shared/FileUploader.js
@@ -13,8 +13,7 @@ import 'filepond/dist/filepond.min.css'
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-import { Button } from '@mui/material'
-// Register the plugins
+import '../../component.css'
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 // Our app
